6a5d8df2667379d38993ecc7cdc4a1442548ba6b
NotJohnnyTamale <81783950+ABUCKY0@users.noreply.github.com>
2024-06-11 17:43:32 -0500

    fix(paths): :bug: Remove example.txt, which was causing errors.

--- src/AutonomousSelector/BuildInfo/build_info.c
+++ src/AutonomousSelector/BuildInfo/build_info.c
@@ -1,11 +1,11 @@
 /* Automatically generated build information */
 #include "build_info.h"
-const char* build_date = "2024-06-09 23:04:01";
-const char* git_branch = "feat/gui-port-from-comptest";
-const char* git_commit = "7b4775ae828ba2616b7ff4549e628eb3bb8a04ee";
+const char* build_date = "2024-06-11 17:42:29";
+const char* git_branch = "main";
+const char* git_commit = "08bf4bbda5b55a0091aad87c5fcc596711ae3e33";
 const char* compiler_version = "arm-none-eabi-g++.exe (GNU Arm Embedded Toolchain 10.3-2021.10) 10.3.1 20210824 (release)";
 const char* build_environment = "Build on MSYS_NT-10.0-22621, x86_64";
-const int build_number = 86;
+const int build_number = 89;
 const char* developer_name = "ABUCKY0";
 const char* application_environment = "alpha";
 const char* codebase_version = "0.0.5";
