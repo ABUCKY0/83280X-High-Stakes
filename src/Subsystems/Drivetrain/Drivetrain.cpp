33517a29bf4319d980d25d1a6710412d965a81f5
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2024-11-24 18:32:04 -0600

    refactor(Intake): remove lift code from intake

--- src/Subsystems/Drivetrain/Drivetrain.cpp
+++ src/Subsystems/Drivetrain/Drivetrain.cpp
@@ -17,8 +17,7 @@ LCHS::Drivetrain::Drivetrain(
     std::initializer_list<std::int8_t> rightDrivePorts) :
     leftDrive(leftDrivePorts),
     rightDrive(rightDrivePorts),
-    intake({MOTOR_PORT_INTAKE, MOTOR_PORT_INTAKE2}, {MOTOR_PORT_LEFT_LIFT, MOTOR_PORT_RIGHT_LIFT},
-           PNEUMATIC_PORT_PTO_LEFT, PNEUMATIC_PORT_PTO_RIGHT, SENSOR_PORT_LIFT),
+    intake({MOTOR_PORT_INTAKE}),
     mogoGrabber(PNEUMATIC_PORT_MOBILE_GOAL, SENSOR_PORT_MOGO_LIMIT_SWITCH) {
 
     }
@@ -53,24 +52,13 @@ void LCHS::Drivetrain::move_velocity(const double velocityLeft,
   rightDrive.move_velocity(velocityRight);
 }
 
-void LCHS::Drivetrain::takeLiftMotors() {
-  this->intake.pto_take();
-}
-
-void LCHS::Drivetrain::releaseLiftMotors() {
-  this->intake.pto_release();
-}
-
 void LCHS::Drivetrain::brake() {
   leftDrive.brake();
   rightDrive.brake();
 }
 
 void LCHS::Drivetrain::driverControl() {
-  if (!reset) {
-    reset = true;
-    this->intake.liftPosition.set_position(0);
-  }
+
   // Get the voltage from the controller
   std::int32_t leftVoltage = -master.get_analog(CONTROL_AXIS_LEFT_DRIVE);
   std::int32_t rightVoltage = master.get_analog(CONTROL_AXIS_RIGHT_DRIVE);
@@ -78,97 +66,6 @@ void LCHS::Drivetrain::driverControl() {
   // Set the voltage for the drivetrain
   //move(leftVoltage, rightVoltage);
   move_velocity(leftVoltage, rightVoltage);
-  // std::cout << "LiftPos: " << this->intake.liftPosition.get_position()
-  //            << std::endl;
 
   bool isLiftMoving = false;
-  // If the driver presses the L1 button, lift moves up.
-  if (this->master.get_digital(CONTROL_BUTTON_LIFT_UP)) {
-    isLiftMoving = true;
-    if (this->intake.getPTOState() == LCHS::PTOState::DRIVETRAIN) {
-      takeLiftMotors();
-    }
-
-    if (!this->intake.isLiftAtTop()) {
-      this->intake.moveLift(-50);
-      std::cout << "moving up,0";
-    } else {
-      this->intake.moveLift(0);
-      this->intake.brakeLift();
-    }
-  } else if (this->master.get_digital(CONTROL_BUTTON_LIFT_DOWN)) {
-    isLiftMoving = true;
-    if (this->intake.getPTOState() == LCHS::PTOState::DRIVETRAIN) {
-      takeLiftMotors();
-    }
-
-    if (!this->intake.isLiftAtBottom()) {
-      this->intake.moveLift(120);
-    } else {
-      this->intake.moveLift(0);
-      this->intake.brakeLift();
-    }
-  } else {
-    this->intake.moveLift(0);
-    this->intake.brakeLift();
-  }
-  // if (this->master.get_digital_new_press(CONTROL_BUTTON_LIFT_DOWN)) {
-  //   switch (this->intake.liftPositionPreset) {
-  //     // enum class LIFTPositionPresets { MAX, WALL, ALLIANCE, MIN, E_STOP };
-  //     case LCHS::LIFTPositionPresets::MAX:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::WALL);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::WALL:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::ALLIANCE);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::ALLIANCE:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::MIN);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::MIN:
-  //       // Do nothing
-  //       break;
-  //     default:
-  //       break; 
-  //   }
-
-  // }
-  // else if (this->master.get_digital_new_press(CONTROL_BUTTON_LIFT_UP)) {
-  //   switch (this->intake.liftPositionPreset) {
-  //     case LCHS::LIFTPositionPresets::MIN:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::ALLIANCE);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::ALLIANCE:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::WALL);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::WALL:
-  //       this->intake.moveLiftPreset(LCHS::LIFTPositionPresets::MAX);
-  //       break;
-  //     case LCHS::LIFTPositionPresets::MAX:
-  //       // Do nothing
-  //       break;
-  //     default:
-  //       break; 
-  //   }
-  // }
-  //   this->intake.update();
-
-  if (this->master.get_digital(CONTROL_BUTTON_INTAKE_OUT)) {
-    this->intake.setIntakeSpeed(127);
-  } else if (master.get_digital(CONTROL_BUTTON_INTAKE_IN)) {
-    this->intake.setIntakeSpeed(-127);
-  } else {
-    this->intake.setIntakeSpeed(0);
-  }
-
-
-  std::atomic_bool mogoGrabbed = false;
-  if (this->master.get_digital_new_press(CONTROL_BUTTON_MOGO_TOGGLE)) {
-    if (mogoGrabbed) {
-      this->mogoGrabber.release();
-      mogoGrabbed = false;
-    } else {
-      this->mogoGrabber.grab();
-      mogoGrabbed = true;
-    }
-  }
 }
--- src/Subsystems/Intake.cpp
+++ src/Subsystems/Intake.cpp
@@ -16,95 +16,23 @@ static float kd_down = 0.0f;
  * This class is responsible for controlling the intake and lift subsystems. It
  * is responsible for controlling the intake motors, lift motors, and the PTOs.
  */
-LCHS::Intake::Intake(std::initializer_list<std::int8_t> motors,
-                     std::initializer_list<std::int8_t> liftmotors,
-                     std::uint8_t pneumaticLeft, std::uint8_t pneumaticRight,
-                     const std::int8_t rotationPort)
-    : intakeMotors(motors), liftMotors(liftmotors),
-      ptoLeft(pneumaticLeft, true), ptoRight(pneumaticRight, true),
-      liftPosition(rotationPort), ptoState(PTOState::LIFT), liftPID(kp_up, ki_up, kd_up, kp_down, ki_down, kd_down) {
-  intakeMotors.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-  liftMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_HOLD);
-  ptoLeft.retract();
-  ptoRight.retract();
-  liftPosition.reset_position();
-}
+LCHS::Intake::Intake(std::initializer_list<std::int8_t> motors): intakeMotors(motors) {}
 
 /**
- * @brief Takes the PTOs from the drivetrain and sets the brake mode to hold
- *
- * Takes the extra 11w motor power from the drivetrain, applies it to the lift,
- * and sets the brake mode to hold
- *
+ * @brief Move the intake motors at a given speed
+ * 
+ * @param speed [-127, 127] The speed to set the intake motors to
+ * @note Uses -127 to 127, i.e. what is outputted by the controller analog sticks
  */
-void LCHS::Intake::pto_take() {
-  // take the PTOs from the drivetrain
-  ptoState = PTOState::LIFT;
-  ptoLeft.retract();
-  ptoRight.retract();
-  ptoLock = false;
-  liftMotors.set_brake_mode_all(pros::E_MOTOR_BRAKE_HOLD);
-  liftMotors.move(0); // Ensure that the lift motors are not moving to prevent
-                      // the lift from having a mind of it's own
-  liftMotors.brake();
-}
-
-void LCHS::Intake::pto_release() {
-  ptoState = PTOState::DRIVETRAIN;
-
-  ptoLeft.extend();
-  ptoRight.extend();
-  ptoLock = true;
-  liftMotors.move(0); // Ensure that the lift motors are not moving, as this
-                      // could negatively affect the drivetrain. Although the
-                      // motors being limp is not ideal, it is better than the
-                      // drivetrain fighting against the motors.
-  /*
-   When connecting to the Drivetrain, we want the motors to coast so that the
-   Drivetrain can move freely. For now, I'm going to leave setting the
-   motorspeed for the controller in the Drivetrain subsystem.
-   */
-  liftMotors.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-}
-
-
-void LCHS::Intake::moveLift( std::int32_t speed) {
-  // liftMotors.move(speed);
-
-  if (ptoState == PTOState::DRIVETRAIN) {
-    pto_take();
-  }
-  pros::delay(15);
-  liftMotors.move_velocity(speed);
-}
-
-
-
-bool LCHS::Intake::isLiftAtBottom() {
-  if (liftPosition.get_position()/100 < 2) {
-    return true;
-  }
-  return false;
-}
-
-bool LCHS::Intake::isLiftAtTop() {
-  if (liftPosition.get_position()/100 > 90) { // 135 degrees is the top of the lift
-    return true;
-  }
-  return false;
-}
-
-LCHS::PTOState LCHS::Intake::getPTOState() { return ptoState; }
-
-void LCHS::Intake::brakeLift() {
-  liftMotors.move_velocity(0);
-  liftMotors.brake();
-}
-
 void LCHS::Intake::setIntakeSpeed(int speed) {
   intakeMotors.move(speed);
 }
 
+/**
+ * @brief Set the Intake to a preset speed
+ * 
+ * @param preset Preset to use
+ */
 void LCHS::Intake::setIntakeSpeedPreset(LCHS::IntakeSpeedPresets preset) {
   switch (preset) {
   case IntakeSpeedPresets::IN:
@@ -121,70 +49,15 @@ void LCHS::Intake::setIntakeSpeedPreset(LCHS::IntakeSpeedPresets preset) {
     break;
   case IntakeSpeedPresets::STOP:
     intakeMotors.move_velocity(0);
+    intakeMotors.brake();
+    break;
+  default:
     break;
   }
 }
 
-
-void LCHS::Intake::moveLiftPreset(LCHS::LIFTPositionPresets preset) {
-  this->liftPositionPreset = preset;
-}
-
-void LCHS::Intake::resetPID() {
-  liftPID.reset();
-}
-
-void LCHS::Intake::update() {
-  // // Update the lift position
-  // LCHS::LIFTPositionPresets current = liftPositionPreset;
-  // int target_pos;
-  // switch (current) {
-  //   case LCHS::LIFTPositionPresets::MAX:
-  //     target_pos = 135; // Max position
-  //     break;
-  //   case LCHS::LIFTPositionPresets::MIN:
-  //     target_pos = 0; // Mid position
-  //     break;
-  //   case LCHS::LIFTPositionPresets::WALL:
-  //     target_pos = 60; // Wall position
-  //     break;
-  //   case LCHS::LIFTPositionPresets::ALLIANCE:
-  //     target_pos = 90; // Alliance position
-  //     break;
-  //   case LCHS::LIFTPositionPresets::E_STOP:
-  //     target_pos = liftPosition.get_position(); // Stop at current position
-  //     break;
-  // }
-  // // Update the lift position based on the current preset
-  // std::cout << "\033[2J";
-  // int target = target_pos - (this->liftPosition.get_position() / 100);
-  // int PIDOutput = liftPID.update(target);
-  // std::cout << "PID Output: " << PIDOutput << std::endl;
-  // std::cout << "Current Position: " << this->liftPosition.get_position() << std::endl;
-  // std::cout << "Get Error: " << target << std::endl;
-  // switch (current) {
-  //   case LCHS::LIFTPositionPresets::MAX:
-  //     std::cout << "Requested State: MAX" << std::endl;
-  //     break;
-  //   case LCHS::LIFTPositionPresets::MIN:
-  //     std::cout << "Requested State: MIN" << std::endl;
-  //     break;
-  //   case LCHS::LIFTPositionPresets::WALL:
-  //     std::cout << "Requested State: WALL" << std::endl;
-  //     break;
-  //   case LCHS::LIFTPositionPresets::ALLIANCE:
-  //     std::cout << "Requested State: ALLIANCE" << std::endl;
-  //     break;
-  //   case LCHS::LIFTPositionPresets::E_STOP:
-  //     std::cout << "Requested State: E_STOP" << std::endl;
-  //     break;
-  // }
-
-  // // Move the lift motors based on the PID output
-  // //this->liftMotors.move_voltage(-PIDOutput);
-
-
-  // //this->liftMotors.move(-PIDOutput);
-  // //PIDOutput = std::clamp(PIDOutput, -100, 100);
-  // this->liftMotors.move(-PIDOutput);
-}
+/**
+ * @brief UNUSED
+ * 
+ */
+void LCHS::Intake::update() {}
