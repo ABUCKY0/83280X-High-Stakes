a82db019576fc5ce2dcedd191cf054c1768d7de0
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2025-01-14 22:21:47 -0600

    refactor(lemlib): :arrow_up: install/update lemlib

--- include/autonomi.hpp
+++ include/autonomi.hpp
@@ -1,12 +1,15 @@
 #ifndef AUTONOMI_HPP
 #define AUTONOMI_HPP
+#include "Subsystems/Drivetrain/Drivetrain.hpp"
+#include "lemlib/api.hpp"  // IWYU pragma: keep
 
-void redLeft();
-void redRight();
-void blueLeft();
-void blueRight();
-void skillsPrimary();
-void skillsSecondary();
+void redLeft(lemlib::Drivetrain& drivetrain, LCHS::Drivetrain& robotsubs);
+void redRight(lemlib::Drivetrain& drivetrain, LCHS::Drivetrain& robotsubs);
+void blueLeft(lemlib::Drivetrain& drivetrain, LCHS::Drivetrain& robotsubs);
+void blueRight(lemlib::Drivetrain& drivetrain, LCHS::Drivetrain& robotsubs);
+void skillsPrimary(lemlib::Drivetrain& drivetrain, LCHS::Drivetrain& robotsubs);
+void skillsSecondary(lemlib::Drivetrain& drivetrain,
+                     LCHS::Drivetrain& robotsubs);
 void noSkills();
 void noMatch();
 
